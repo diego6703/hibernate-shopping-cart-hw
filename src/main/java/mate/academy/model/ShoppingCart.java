8842600805d4e7e74b0e051b97b8bd99c88e4499
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-06-19 17:15:51 +0200

    hibernate-shopping-cart-hw v1

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,43 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.util.List;
+
+@Entity
+public class ShoppingCart {
+    @Id
+    private Long id;
+    @OneToMany
+    private List<Ticket> tickets;
+    @MapsId
+    @OneToOne
+    private User user;
+
+    public ShoppingCart() {
+    }
+
+    public ShoppingCart(List<Ticket> tickets, User user) {
+        this.tickets = tickets;
+        this.user = user;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/mate/academy/model/Ticket.java
@@ -0,0 +1,26 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private MovieSession movieSession;
+    @ManyToOne
+    private User user;
+
+    public Ticket() {
+    }
+
+    public Ticket(MovieSession movieSession, User user) {
+        this.movieSession = movieSession;
+        this.user = user;
+    }
+}
